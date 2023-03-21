9b086a136711ffcb6a5472ee5964cdc4841d078b
Sebastian <115455174+sebawoz02@users.noreply.github.com>
2023-03-21 18:29:18 +0100

    Update z3Kosaraju.cpp

--- l1/SCC/z3Kosaraju.cpp
+++ l1/SCC/z3Kosaraju.cpp
@@ -10,38 +10,48 @@ using namespace std;
 
 class Graph{
 private:
-    void DFS(bool visited[], int startVertex, bool print, list<int> *stack){
+    // step 1 dfs
+    void DFS(bool* visited, int startVertex, list<int> *stack){
         visited[startVertex] = true;
-        if(print) cout << startVertex + 1 << ", ";
-        list<int> adj = verticesList[startVertex];
-        list<int>::iterator i;
-        for(i = adj.begin(); i!=adj.end(); i++){
-            if(!visited[*i]) DFS(visited, *i, print, stack);
+        for(int & i : verticesList[startVertex]){
+            if(!visited[i]) DFS(visited, i, stack);
         }
-        if(!print) stack->push_front(startVertex);
+        stack->push_front(startVertex);
     }
 
-    Graph getTranspose(){
-        Graph transpose(this->numOfVertices);
+    // step 2
+    Graph* getTranspose(){
+        auto* transpose = new Graph(this->numOfVertices);
         for(int v = 0; v<numOfVertices; v++){
-            list<int>::iterator i;
-            for(i = verticesList[v].begin(); i!=verticesList[v].end(); i++){
-                transpose.addEgde(*i+1, v+1);
+            for(auto i = verticesList[v].begin(); i!=verticesList[v].end(); i++){
+                transpose->addEgde(*i+1, v+1);
             }
         }
         return transpose;
     }
 
+    // step 3 dfs
+    void dfsT(int vertex, bool* visited){
+        visited[vertex] = true;
+        if(numOfVertices<=200) cout << vertex + 1 << ", ";
+        for(int & i : verticesList[vertex]){
+            if(!visited[i]) dfsT(i, visited);
+        }
+    }
+
+
+
 public:
-    map<int, list<int>> verticesList;
+    list<int>* verticesList;
     int numOfVertices;
 
 
     explicit Graph(int V){
         this->numOfVertices = V;
+        verticesList = new list<int>[V];
     }
 
-    void addEgde(int v, int u){
+    void addEgde(int v, int u) const{
         verticesList[v-1].push_back(u-1);
     }
 
@@ -52,12 +62,10 @@ public:
         for(int i = 0; i< numOfVertices; i++) visited[i] = false;
 
         // step 1: fill order
-        for(int i = 0; i< numOfVertices; i++){
-            if(!visited[i]) DFS(visited, i, false, &stack);
-        }
+        for(int i = 0; i< numOfVertices; i++) if(!visited[i]) DFS(visited, i, &stack);
 
         // step 2: get transpose graph
-        Graph tr = getTranspose();
+        Graph* tr = getTranspose();
         for(int i = 0; i< numOfVertices; i++) visited[i] = false;
 
         // step 3: process vertices on stack
@@ -66,7 +74,7 @@ public:
             stack.pop_front();
             // print ssc
             if(!visited[v]){
-                tr.DFS(visited, v, true, &stack);
+                tr->dfsT(v, visited);
                 cout << "/"<< endl;
             }
         }
@@ -76,7 +84,6 @@ public:
 };
 
 
-
 int main(){
     // Wczytywanie danych z pliku
     clock_t start = clock();
@@ -84,12 +91,10 @@ int main(){
     string line;
     string space_delimiter = " ";
 
-    ifstream file("E:/STUDIA/AOD/list1/project1/aod_testy1/3/g3-5.txt");
+    ifstream file("E:/STUDIA/AOD/list1/project1/aod_testy1/3/g3-4.txt");
 
     int i = 1;
     int V;
-
-
     Graph *graph;
 
     while(getline(file, line)){
@@ -107,7 +112,6 @@ int main(){
             }
             values.push_back(line);
             graph->addEgde(stoi(values[0]), stoi(values[1]));
-
         }
         i++;
 
@@ -117,7 +121,5 @@ int main(){
     cout << (end - start)/(double)CLOCKS_PER_SEC << "s"<< endl;
     // Wczytano dane
 
-
     graph->findSSC();
-
 }
